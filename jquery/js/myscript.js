59e782e018856e1e7335bfd276acaab2384fe6b4
Kensei Tron <kenseitron@gmail.com>
2021-08-29 18:41:29 -0500

    Web Design 2

---
+++ jquery/js/myscript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('#show-me').click(function() {
+        $('#new-buttons').addClass('show');
+    })
+
+    $('#get-started').click(function() {
+        $('#blue-block').removeClass('bg-primary');
+        $('#blue-content').removeClass('text-white');
+    })
+
+    setTimeout(function () {
+        $('#shop-button').fadeIn(500);
+        }, 3000);
+})
